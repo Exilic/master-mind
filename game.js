200f13c28f281069df74d87832ad2dfa1d62b746
Exilic <tobias.engberg@exilic.se>
2022-11-10 15:39:23 +0100

    Basic four circles

---
+++ game.js
@@ -0,0 +1,82 @@
+/*
+const Colors = {
+    Yellow: 'Yellow',
+    Red: 'Red',
+    Blue: 'Blue',
+    Green: 'Green',
+    White: 'White',
+    Cyan: 'Cyan'
+};*/
+
+//const target = Colors[4];
+//const trials = Colors[12][4];
+
+// How to store the array of correct colors?
+
+// How to best use the Colors object in the random generator?
+
+// How to mark a try?
+// How to choose color for the marking?
+// How "hand in" a try?
+
+let xmlns = "http://www.w3.org/2000/svg";
+
+// Generate four divs and set them to colors
+function inputPart(){
+    let insertionPoint = document.getElementById("insertion");
+    for(let i = 0; i < 4; i++){
+        let newElement = document.createElement("div");
+        newElement.classList.add("choice");
+        insertionPoint.append(newElement);
+    }
+    makeSVG();
+    makeCircle("50", "50");
+    makeCircle("120", "50");
+    makeCircle("190", "50");
+    makeCircle("260", "50");
+}
+
+function makeSVG(){
+
+    let insertionPoint = document.getElementById("insertion");
+    let newElement = document.createElementNS(xmlns, "svg");
+    newElement.setAttribute("height", "100"  );
+    newElement.setAttribute("width", "400"  );
+    newElement.classList.add("actualchoice");
+    insertionPoint.append(newElement);
+}
+
+function makeCircle(cx, cy){
+    let insertionPoint = document.getElementsByClassName("actualchoice");
+    let newElement = document.createElementNS(xmlns, "circle");
+    newElement.classList.add("circle");
+    newElement.setAttribute("cx", cx  );
+    newElement.setAttribute("cy", cy  );
+    newElement.setAttribute("r", "20"  );
+    newElement.setAttribute("fill", "blue"  );
+    newElement.setAttribute("stroke", "blue"  );
+    insertionPoint[0].append(newElement);
+}
+/*
+
+function generateTargetSequence(){
+
+}
+
+
+function evaluateTry(){
+    let blacks = determineBlacks();
+    let whites = determineWhites();
+
+}
+
+function determineBlacks(){
+    return "all the black ones";
+}
+
+function determineWhites(){
+    return "all the white ones";
+}
+*/
+
+inputPart();
